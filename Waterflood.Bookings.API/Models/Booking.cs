a0cc498a424c10c617f706dc5e8f8db66bc8361e
Charn-98 <45961589+Charn-98@users.noreply.github.com>
2025-09-15 18:23:34 +0200

    Booking feature added (#1)
    * Added initial minimal API for bookings
    
    * Added unit tests to project
    
    * Added readme
    
    ---------
    
    Co-authored-by: charn <charnv@voxcroft.com>

---
+++ Waterflood.Bookings.API/Models/Booking.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Waterflood.Bookings.API.Models
+{
+    public class Booking
+    {
+        public Guid Id { get; set; }
+        public string CustomerName { get; set; } = string.Empty;
+        public BookingTypeEnum BookingType { get; set; }
+        public DateTimeOffset StartDate { get; set; }
+        public DateTimeOffset EndDate { get; set; }
+    }
+}
